df751e4f916631a334a0046b6e04015d3e626cd2
chau03 <thanhchau16072003@gmail.com>
2025-03-24 18:07:41 +0700

    update crud  permison&role

--- src/main/java/com/example/demo/controller/ResumeController.java
+++ src/main/java/com/example/demo/controller/ResumeController.java
@@ -2,9 +2,12 @@ package com.example.demo.controller;
 
 import java.util.Optional;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -13,11 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Resume;
+import com.example.demo.domain.response.ResultPaginationDTO;
 import com.example.demo.domain.response.resume.ResCreateResumeDTO;
+import com.example.demo.domain.response.resume.ResFetchResumeDTO;
 import com.example.demo.domain.response.resume.ResUpdateResumeDTO;
 import com.example.demo.service.ResumeService;
 import com.example.demo.util.annotation.ApiMessage;
 import com.example.demo.util.error.IdInvalidException;
+import com.turkraft.springfilter.boot.Filter;
 
 import jakarta.validation.Valid;
 
@@ -67,5 +73,25 @@ public class ResumeController {
 
         this.resumeService.delete(id);
         return ResponseEntity.ok().body(null);
+    }
+
+    @GetMapping("/resumes/{id}")
+    @ApiMessage("Fetch a resume by id")
+    public ResponseEntity<ResFetchResumeDTO> fetchById(@PathVariable("id") long id) throws IdInvalidException {
+        Optional<Resume> reqResumeOptional = this.resumeService.fetchById(id);
+        if (reqResumeOptional.isEmpty()) {
+            throw new IdInvalidException("Resume với id = " + id + " không tồn tại");
+        }
+
+        return ResponseEntity.ok().body(this.resumeService.getResume(reqResumeOptional.get()));
+    }
+
+    @GetMapping("/resumes")
+    @ApiMessage("Fetch all resume with paginate")
+    public ResponseEntity<ResultPaginationDTO> fetchAll(
+            @Filter Specification<Resume> spec,
+            Pageable pageable) {
+
+        return ResponseEntity.ok().body(this.resumeService.fetchAllResume(spec, pageable));
     }
 }
---
+++ src/main/java/com/example/demo/repository/PermissionRepository.java
@@ -0,0 +1,18 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.Permission;
+
+@Repository
+public interface PermissionRepository extends JpaRepository<Permission, Long>,
+        JpaSpecificationExecutor<Permission> {
+
+    boolean existsByModuleAndApiPathAndMethod(String module, String apiPath, String method);
+
+    List<Permission> findByIdIn(List<Long> id);
+}
